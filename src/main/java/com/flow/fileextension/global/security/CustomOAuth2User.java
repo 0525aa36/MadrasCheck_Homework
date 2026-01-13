e930b3bbe61755e83cbe79fd0c397c40edcc94c7
0525aa36 <0525aa36@gmail.com>
2026-01-13 15:12:39 +0900

    feat: CustomOAuth2User 구현

---
+++ src/main/java/com/flow/fileextension/global/security/CustomOAuth2User.java
@@ -0,0 +1,46 @@
+package com.flow.fileextension.global.security;
+
+import com.flow.fileextension.domain.user.entity.User;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+@Getter
+public class CustomOAuth2User implements OAuth2User, Serializable {
+    private final User user;
+    private final Map<String, Object> attributes;
+
+    public CustomOAuth2User(User user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getName() {
+        return user.getName();
+    }
+
+    public String getEmail() {
+        return user.getEmail();
+    }
+
+    public String getPicture() {
+        return user.getProfileImage();
+    }
+}
